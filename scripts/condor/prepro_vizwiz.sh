b8065aca4b877a11b73e8c1cd93a873b91eea515
Jie Hao Liao <liaojh1998@gmail.com>
2021-05-02 08:01:46 -0500

    Fixed not creating a directory to receive preprocessing output

--- scripts/condor/prepro_vizwiz.sh
+++ scripts/condor/prepro_vizwiz.sh
@@ -5,18 +5,25 @@ USERNAME=$1
 set -ex
 
 for SPLIT in "train" "val" "test"; do
-    if [ ! -d ./data/uniter/vizwiz/${SPLIT}/image ]; then
-        mkdir -p ./data/uniter/vizwiz/${SPLIT}/image
-    fi
-
+    # Text DB
     singularity exec -B ./data/:/data,./UNITER:/src \
         --nv -w /scratch/cluster/${USERNAME}/uniter bash -c \
         "cd /src && python3 prepro.py --annotations /data/vizwiz/Annotations/${SPLIT}.json \
             --output /data/uniter/vizwiz/${SPLIT}/text \
             --task vizwiz"
+
+    # Image features
+    if [ ! -d ./data/uniter/vizwiz/${SPLIT}/image_feat ]; then
+        mkdir -p ./data/uniter/vizwiz/${SPLIT}/image_feat
+    fi
     singularity exec -B ./data/vizwiz/${SPLIT}:/img,./data/uniter/vizwiz/${SPLIT}/image_feat/:/output \
         --nv -w /scratch/cluster/${USERNAME}/uniter_butd bash -c \
         "cd /src && python tools/generate_npz.py --gpu 0"
+
+    # Image DB
+    if [ ! -d ./data/uniter/vizwiz/${SPLIT}/image ]; then
+        mkdir -p ./data/uniter/vizwiz/${SPLIT}/image
+    fi
     singularity exec -B ./data/:/data,./UNITER:/src \
         --nv -w /scratch/cluster/${USERNAME}/uniter bash -c \
         "cd /src && python3 scripts/convert_imgdir.py --img_dir /data/uniter/vizwiz/${SPLIT}/image_feat \
