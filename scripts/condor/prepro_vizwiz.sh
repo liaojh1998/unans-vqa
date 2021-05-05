507203365394095e5b968927a5ab9a126a1058b4
Jie Hao Liao <liaojh1998@gmail.com>
2021-05-05 02:34:46 -0500

    Created a new train and validation split, and use old val for evaluations instead

--- scripts/condor/prepro_vizwiz.sh
+++ scripts/condor/prepro_vizwiz.sh
@@ -11,9 +11,25 @@ for SPLIT in "train" "val" "test"; do
         "cd /src && python3 prepro.py --annotations /data/vizwiz/Annotations/${SPLIT}.json \
             --output /data/uniter/vizwiz/${SPLIT}/text \
             --task vizwiz"
+    if [[ ${SPLIT} == "train" ]]; then
+        if [[ -f ./data/vizwiz/Annotations/${SPLIT}_new.json ]]; then
+            singularity exec -B ./data/:/data,./UNITER:/src \
+                --nv -w /scratch/cluster/${USERNAME}/uniter bash -c \
+                "cd /src && python3 prepro.py --annotations /data/vizwiz/Annotations/${SPLIT}_new.json \
+                    --output /data/uniter/vizwiz/${SPLIT}/text_new \
+                    --task vizwiz"
+        fi
+        if [[ -f ./data/vizwiz/Annotations/${SPLIT}_val.json ]]; then
+            singularity exec -B ./data/:/data,./UNITER:/src \
+                --nv -w /scratch/cluster/${USERNAME}/uniter bash -c \
+                "cd /src && python3 prepro.py --annotations /data/vizwiz/Annotations/${SPLIT}_val.json \
+                    --output /data/uniter/vizwiz/${SPLIT}/text_val \
+                    --task vizwiz"
+        fi
+    fi
 
     # Image features
-    if [ ! -d ./data/uniter/vizwiz/${SPLIT}/image/all ]; then
+    if [[ ! -d ./data/uniter/vizwiz/${SPLIT}/image/all ]]; then
         mkdir -p ./data/uniter/vizwiz/${SPLIT}/image/all
     fi
     singularity exec -B ./data/vizwiz/${SPLIT}:/img,./data/uniter/vizwiz/${SPLIT}/image/all:/output \
@@ -21,7 +37,7 @@ for SPLIT in "train" "val" "test"; do
         "cd /src && python -u tools/generate_npz.py --gpu 0"
 
     # Image DB
-    if [ ! -d ./data/uniter/vizwiz/${SPLIT}/tmp ]; then
+    if [[ ! -d ./data/uniter/vizwiz/${SPLIT}/tmp ]]; then
         mkdir -p ./data/uniter/vizwiz/${SPLIT}/tmp
     fi
     singularity exec -B ./data/:/data,./UNITER:/src \
