970b7c12793e202a13fecde0548f061caf6f95a0
Jie Hao Liao <liaojh1998@gmail.com>
2021-05-07 06:28:35 -0500

    Added inference script

---
+++ scripts/condor/run_inf.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+USERNAME=$1
+CONFIG=$2
+
+singularity exec -B /scratch/cluster/${USERNAME}/gnlp/unans-vqa/:/src,/scratch/cluster/${USERNAME}/gnlp/unans-vqa/data:/data \
+    --nv -w /scratch/cluster/${USERNAME}/uniter bash -c \
+    "cd /src/UNITER && python3 inf_unans.py --config config/${CONFIG}"
